3414c7bf9d7b72c2d0a2219122cae6e00de1de9c
leitoon <jorgeleiar@gmail.com>
2023-10-05 14:14:12 -0500

    mapa

--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,10 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_app/providers/dbProvider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Mapa_page extends StatelessWidget {
+class Mapa_page extends StatefulWidget {
+  
+  @override
+  State<Mapa_page> createState() => _Mapa_pageState();
+}
+
+class _Mapa_pageState extends State<Mapa_page> {
+  Completer<GoogleMapController> _controller=Completer();
   
   @override
   Widget build(BuildContext context) {
+
+   final CameraPosition puntoInicial = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
      final arguments = ModalRoute.of(context)!.settings.arguments;
  
     if (arguments != null) {
@@ -12,9 +27,13 @@ class Mapa_page extends StatelessWidget {
  
       return Scaffold(
         appBar: AppBar(title: const Text('Mapa')),
-        body: Center(
-          child: Text(scan.valor),
-        ),
+        body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
       );
     }
  
